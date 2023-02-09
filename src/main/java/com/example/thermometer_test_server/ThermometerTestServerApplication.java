f0664a972a6f4ccfe45a3333594bc1421836e60c
Makar <makardrozdik00@gmail.com>
2023-02-09 15:12:18 +0200

    Initial commit

---
+++ src/main/java/com/example/thermometer_test_server/ThermometerTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.thermometer_test_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThermometerTestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThermometerTestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thermometer_test_server/ThermometerTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thermometer_test_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThermometerTestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
